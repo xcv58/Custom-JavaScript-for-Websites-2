ea447d8478c7b7f7529403cf4f752c1113ba7e27
xcv58 <chenyihonglove@gmail.com>
2015-04-15 15:58:54 -0400

    use chrome storage sync instead of localStorage

--- extension/lib/api.js
+++ extension/lib/api.js
@@ -1,24 +1,29 @@
 (function(chrome) {
-    chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-        switch(request.method) {
-            case 'setData':
-                localStorage['customjs'] = JSON.stringify(request.customjs);
-            case 'getData':
-                var customjs = JSON.parse(localStorage['customjs'] || 'false');
-                sendResponse({customjs: customjs, host: location.host, protocol: location.protocol});
-                break;
-            case 'removeData':
-                delete localStorage['customjs'];
-                break;
-            case 'goTo':
-                window.location = request.link;
-                break;
-            default:
-                sendResponse({src: '', config: {}});
-        }
-        if( request.reload ) {
-            window.location.reload();
-        }
-    });
+  chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+    var website = location.protocol + '//' + location.host;
+    switch(request.method) {
+    case 'setData':
+      var syncdata =  {};
+      syncdata[website] = request.customjs;
+      chrome.storage.sync.set(syncdata);
+    case 'getData':
+      chrome.storage.sync.get(website, function(obj) {
+        var customjs = obj[website] || JSON.parse('false');
+        sendResponse({customjs: customjs, host: location.host, protocol: location.protocol});
+      });
+      break;
+    case 'removeData':
+      chrome.storage.sync.remove(website, function() {});
+      break;
+    case 'goTo':
+      window.location = request.link;
+      break;
+    default:
+      sendResponse({src: '', config: {}});
+    }
+    if( request.reload ) {
+      window.location.reload();
+    }
+  });
 
 })(chrome);
--- extension/lib/run.js
+++ extension/lib/run.js
@@ -1,36 +1,35 @@
 (function() {
-    function injectScript(src, where) {
-        var elm = document.createElement('script');
-        elm.src = src;
-        document[where || 'head'].appendChild(elm);
-    }
+  function injectScript(src, where) {
+    var elm = document.createElement('script');
+    elm.src = src;
+    document[where || 'head'].appendChild(elm);
+  }
 
-    var customjs = localStorage['customjs'];
+  var website = location.protocol + '//' + location.host;
+  chrome.storage.sync.get(website, function(obj) {
+    var customjs = obj[website];
     if( customjs ) {
-        customjs = JSON.parse(customjs);
-
-        if( customjs.config.enable ) {
-
-            // Predefined include
-            if( customjs.config.include ) {
-                injectScript('https://ajax.googleapis.com/ajax/libs' + customjs.config.include);
-            }
-
-            // Extra include
-            var extra = (customjs.config.extra || '').split(';');
-            extra.forEach(function(line) {
-                if( line.substr(0, 1) !== '#' ) {
-                    injectScript(line);
-                }
-            });
-
-            // Script
-            if( customjs.source ) {
-                setTimeout(function() {
-                    injectScript(customjs.source, 'body');
-                }, 250);
-            }
-
+      if( customjs.config.enable ) {
+        // Predefined include
+        if( customjs.config.include ) {
+          injectScript('https://ajax.googleapis.com/ajax/libs' + customjs.config.include);
         }
+
+        // Extra include
+        var extra = (customjs.config.extra || '').split(';');
+        extra.forEach(function(line) {
+          if( line.substr(0, 1) !== '#' ) {
+            injectScript(line);
+          }
+        });
+
+        // Script
+        if( customjs.source ) {
+          setTimeout(function() {
+            injectScript(customjs.source, 'body');
+          }, 250);
+        }
+      }
     }
+  });
 })();
