fb5267c1e959afc3a625dddecb6c2d675c1e84a3
xcv58 <i@xcv58.com>
2017-10-24 20:06:44 -0700

    Add confirm alert before reset
    Fix #18

--- src/js/components/Reset.js
+++ src/js/components/Reset.js
@@ -1,23 +1,47 @@
 import React, { Component } from 'react'
 import { inject, observer } from 'mobx-react'
+import Button from 'material-ui/Button'
+import Dialog, {
+  DialogActions, DialogContent, DialogContentText, DialogTitle
+} from 'material-ui/Dialog'
 
 @inject('AppStore')
 @observer
 export default class Reset extends Component {
-  onReset = (e) => {
-    e.preventDefault()
+  state = { open: false }
+
+  openDialog = () => this.setState({ open: true })
+
+  closeDialog = () => this.setState({ open: false })
+
+  handleReset = () => {
     this.props.AppStore.reset()
+    this.closeDialog()
   }
 
   render () {
-    const { differentURL, tabMode } = this.props.AppStore
-    return (
-      <input onClick={this.onReset}
-        disabled={differentURL && !tabMode}
-        type='submit'
-        className='controls__reset pure-button'
-        value='reset'
-      />
-    )
+    const { domain } = this.props.AppStore
+    return [
+      <Button raised onClick={this.openDialog}>
+        Reset
+      </Button>,
+      <Dialog open={this.state.open} onRequestClose={this.closeDialog}>
+        <DialogTitle>Remove all codes and external scripts?</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Reset will remove your codes and external scripts, and delete
+            current domain: {domain} from domain lists.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button raised onClick={this.closeDialog}>
+            Cancel
+          </Button>
+          <Button raised color='primary' onClick={this.handleReset}>
+            Reset
+          </Button>
+        </DialogActions>
+      </Dialog>
+    ]
   }
 }
