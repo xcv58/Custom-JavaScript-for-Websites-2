919edd11c99bd17b68b5c1c00696de727f76e0bd
xcv58 <i@xcv58.com>
2017-10-25 22:19:51 -0700

    Use Material UI for Include

--- src/js/components/Include.js
+++ src/js/components/Include.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import { inject, observer } from 'mobx-react'
+import { InputLabel } from 'material-ui/Input'
+import { MenuItem } from 'material-ui/Menu'
+import { FormControl } from 'material-ui/Form'
+import Button from 'material-ui/Button'
+import Select from 'material-ui/Select'
 
 @inject('IncludeStore')
 @observer
 export default class Include extends Component {
   onSelect = (e) => {
-    e.preventDefault()
     this.props.IncludeStore.onSelect(e.target.value)
   }
 
@@ -15,25 +19,30 @@ export default class Include extends Component {
 
   render () {
     const { include, includes, extraOpen, toggleExtraOpen, extraValue } = this.props.IncludeStore
+    const options = includes.map(({ name, path }) => (
+      <MenuItem key={name} value={path}>{name}</MenuItem>
+    ))
     return (
-      <div className='pure-u-5-6 include'>
-        <div className='include__body'>
-          You can inject <a href='#' onClick={toggleExtraOpen}>your own external scripts</a> or
-          <label htmlFor='include'> predefined one:</label>
-          <select
-            name='include'
-            value={include}
-            onChange={this.onSelect}
-          >
-            <option value=''> ---- nothing ---- </option>
-            {
-              includes.map(({ name, path }) => (
-                <option key={name} value={path}>
-                  {name}
-                </option>
-              ))
-            }
-          </select>
+      <div>
+        <div>
+          <InputLabel>You can inject </InputLabel>
+          <Button color='primary'
+            onClick={toggleExtraOpen}
+            style={{ padding: 0, textTransform: 'none' }}>
+            your own external scripts
+          </Button>
+          <InputLabel> or predefined one: </InputLabel>
+          <FormControl>
+            <Select value={include}
+              displayEmpty
+              autoWidth
+              onChange={this.onSelect}>
+              <MenuItem value=''>
+                <em>None</em>
+              </MenuItem>
+              {options}
+            </Select>
+          </FormControl>
         </div>
         <div className={`include__popbox ${extraOpen ? '' : 'is-hidden'}`}>
           <div className='include__popbox__body'>
